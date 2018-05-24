32c4adb9f0690c377cdbb7a7dab5ba5257acc0cd
Ak-lee <laklc@126.com>
2018-05-24 23:09:40 +0800

    update

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,26 @@
-import './member_base.css'
-import './member.css'
+
+//  1. 使用 vue-router 插件
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)       //  使用路由插件
+
+//  2. 创建 router 实例
+let routes = [{
+  path: '/',
+  component: require('./components/member.vue').default
+}]
+let router = new Router({
+  routes
+})
+
+//  3.根组件的注入
+new Vue({
+  el: '#app',
+  router,
+  data:{
+
+  },
+  methods:{
+
+  }
+})
